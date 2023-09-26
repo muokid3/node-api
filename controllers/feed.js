36e09f8042c6f26025e7c50e8b5581ef34704585
Dennis Muoki <muokid3@gmail.com>
2023-09-26 12:00:50 +0300

    update post

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,4 +1,6 @@
 const { validationResult } = require("express-validator");
+const fs = require("fs");
+const path = require("path");
 
 const Post = require("../models/post");
 
@@ -90,4 +92,55 @@ exports.getPost = (req, res, next) => {
       }
       next(err);
     });
+};
+
+exports.updatePost = (req, res, next) => {
+  const postId = req.params.postId;
+  const title = req.body.title;
+  const content = req.body.content;
+  let imageUrl = req.body.image;
+
+  if (req.file) {
+    imageUrl = req.file.path;
+  }
+
+  if (!imageUrl) {
+    const err = new Error("Image is missing");
+    err.statusCode = 422;
+    throw err;
+  }
+
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const err = new Error("Post not found");
+        err.statusCode = 404;
+        throw err;
+      }
+      if (imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl);
+      }
+
+      post.title = title;
+      post.imageUrl = imageUrl;
+      post.content = content;
+      return post.save();
+    })
+    .then((result) => {
+      res.status(200).json({
+        message: "Post has been updated",
+        post: result,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, "..", filePath);
+  fs.unlink(filePath, (err) => console.log(err));
 };
